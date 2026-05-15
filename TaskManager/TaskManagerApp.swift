d7cb62218de74bdf1a413deb336ae9c13409d0d9
Tuyenkaka2005 <nguyendangtuyen2005@gmail.com>
2026-05-15 02:45:09 -0700

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Tuyen on 15/5/26.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
